6595f07f8162b2a85070127c76c1889bf163d7a3
S528737 <bunty999>
2017-10-11 22:00:49 -0500

    final

--- server.js
+++ server.js
@@ -3,7 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var router = express.Router();
+var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
 
+var url = 'mongodb://localhost:27017/flight';
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
@@ -19,49 +30,19 @@ var url = 'mongodb://localhost:27017/flight';
 app.use(express.static(__dirname ));
 
 app.get('/index',function(req,res){
-	res.sendFile(__dirname+"public_html/index");
+	res.sendFile(__dirname+"/index.html");
 	
 });
 
-//For fetching the data from the mongoDB
-app.get('/flightDetails', function(req,res){
-    var newUser = {
-	email: req.query.email
-	};
-        console.log(req.query.email);
-    mongoClient.connect(url,function(err,db){
-		if (err){
-	console.error('Error occured in database');
-	res.send("Error in connection");
-
-} else{
-	console.log('Connection established '+ url);
-db.collection('users').findOne({email:newUser.email},function(err,result){
-if (err){console.log(err);}
-else{
-    console.log("printing results....");
- console.log(result);
- 
- res.send(result.firstname+" "+result.lastname+" ("+result.phone+")" +"subscribed to newsletter");
-	}
-        });
-        }
-    });
-});
-//for inserting the data into mongoDB
 app.post('/index',function(req,res){
-    
-    console.log(req);
 	var newUser = {
 	email: req.body.email,
 	firstname: req.body.firstname,
 	lastname:req.body.lastname,
 	phone:req.body.phone,        
-//   date: req.body.date,
-//   dateTo: req.body.dateTo
-   
+   date: req.body.dt,
+   dateTo: req.body.dt1
 	};
-        console.log(newUser);
 	mongoClient.connect(url,function(err,db){
 		if (err){
 	console.error('Error occured in database');
@@ -79,12 +60,13 @@ db.collection('users').insert(newUser,function(err,result){
 	console.log(err);
 	} else {
 	console.log('Item Inserted');
-        res.send("Inserted records successfully "+newUser.email);
-						db.close();
+         res.send('Inserted records successfully');
+	 db.close();
 					}
 					});
 
-	} else {
+	} 
+        else {
 	res.send("already exists");
    db.close();
 
@@ -100,11 +82,17 @@ db.collection('users').insert(newUser,function(err,result){
 
 });
 
+app.listen(3000);
+console.log('Running on port 3000');
 
-app.set('port', 3009);
-app.listen(app.set('port'), function(){
-console.log('Server is running at port: ' + app.get('port'));
-});
+http.createServer(function(request ,response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Page One');
+}).listen(80);
+console.log("Server is listening");
 
 
-    
+
+
+
+
